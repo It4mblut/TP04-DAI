8a2a260bbb1a885117e60b73badcaef9d07ef449
EITAN URIEL KOZE <49433681@ort>
2026-04-27 09:07:16 -0300

    Avances chiquitos

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 //(index.js) punto de entrada del servido//
+import app from './src/app.js'
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor en puerto https://localhost:${port}`)
+})
--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
 import express from 'express';
+import horaActual from './time.js'
 //(app.js) Configuración de Express y rutas//
 
+const app = express();
+
+
+app.get('/',(req,res)=> {
+res.send('hoolaaa');
+
+app.get('/hora',(req,res)=> {
+res.send(horaActual);
+
+app.get('/fecha-completa', (req, res) => {
+    const fecha = new Date();
+    const opciones = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    res.send(fecha.toLocaleDateString('es-ES', opciones));
+});
+
+})
+})
+export default app;
